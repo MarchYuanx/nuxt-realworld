8d1036f25f9d87cd174106a003060166faa5b2ce
MarchYuanx <592302815@qq.com>
2021-03-24 23:01:39 +0800

    host & port

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,5 +52,9 @@ module.exports = {
     plugins: [
         '~/plugins/request.js',
         '~/plugins/dayjs.js',
-    ]
+    ],
+    server: {
+        host: '0.0.0.0',
+        port: 3000
+    },
 }
